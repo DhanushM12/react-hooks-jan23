d9aaa0a3d2b22c38452eb4d6b4aff827d382ff20
DhanushM12 <dhanushmukhi@gmail.com>
2023-10-18 22:08:03 +0530

    added hooks

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import UseCallback from './hooks/UseCallback';
+import UseContext from './hooks/UseContext';
 import UseEffect from './hooks/UseEffect';
 import UseMemo from './hooks/UseMemo';
 import UseRef from './hooks/UseRef';
@@ -13,6 +14,7 @@ function App() {
         <UseRef />
         <UseMemo />
         <UseCallback />
+        <UseContext />
     </div>
   );
 }
