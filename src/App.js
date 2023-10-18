aeb59ebc1662b6f91324ec2668b97e24f89d1851
DhanushM12 <dhanushmukhi@gmail.com>
2023-10-18 22:18:52 +0530

    routes

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import UseCallback from './hooks/UseCallback';
 import UseContext from './hooks/UseContext';
@@ -5,16 +6,26 @@ import UseEffect from './hooks/UseEffect';
 import UseMemo from './hooks/UseMemo';
 import UseRef from './hooks/UseRef';
 import UseState from './hooks/UseState';
+import Login from './components/Login';
+import Register from './components/Register';
+import Home from './components/Home';
+import Notfound from './components/Notfound';
 
 function App() {
   return (
     <div className="App">
-        <UseState />
+        {/* <UseState />
         <UseEffect />
         <UseRef />
         <UseMemo />
         <UseCallback />
-        <UseContext />
+        <UseContext /> */}
+        <Routes>
+          <Route path='/' element={<Home />}/>
+          <Route path='/login' element={<Login />}/>
+          <Route path='/register' element={<Register />}/>
+          <Route path='*' element={<Notfound />} />
+        </Routes>
     </div>
   );
 }
